85cf456b683d75d76250e2ec5e7dc2b0a0b84595
ferru97 <vitof970@gmail.com>
2023-04-15 18:39:42 +0200

    [VF] Initialized user microservice

---
+++ S4F-user-microservice/src/test/java/com/ferru97/s4f/users/S4F/user/microservice/S4FUserMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferru97.s4f.users.S4F.user.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S4FUserMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
