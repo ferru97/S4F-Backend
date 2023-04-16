068fd697904f0f14ea58e88def1dcc7b9e09df7a
ferru97 <vitof970@gmail.com>
2023-04-16 17:11:35 +0200

    [VF] Added checkstyle

---
+++ S4F-user-microservice/src/main/java/com/ferru97/s4fuser/s4f/user/microservice/S4FUserMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ferru97.s4fuser.s4f.user.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S4FUserMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S4FUserMicroserviceApplication.class, args);
+	}
+
+}
